93746fd453d6dd34df67a5ede8c7d8acc4fa7d6e
cohalz <cohalz@hatena.ne.jp>
2019-06-14 21:54:09 +0900

    download on startup

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apk add --no-cache python3 && \
     wget -O - https://bootstrap.pypa.io/get-pip.py | python3 && \
     pip install awscli
 
-COPY s3-sync.sh /usr/local/bin
+COPY s3-sync.sh /
 COPY crontab /var/spool/cron/crontabs/root
 
-CMD ["crond", "-f", "-d", "8"]
+CMD ["sh", "-c", "/s3-sync.sh && crond -f -d 8"]
--- s3-sync.sh
+++ s3-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "${SYNC_TYPE}" = "PULL" ]; then
-   aws s3 sync --exact-timestamps --delete s3://"${S3_BUCKET}"/ "${LOCAL_DIR}"
+   aws s3 sync --exact-timestamps --delete s3://"${S3_BUCKET}" "${LOCAL_DIR}"
 elif [ "${SYNC_TYPE}" = "PUSH" ]; then
-   aws s3 sync --exact-timestamps --delete "${LOCAL_DIR}" s3://"${S3_BUCKET}"/
+   aws s3 sync --exact-timestamps --delete "${LOCAL_DIR}" s3://"${S3_BUCKET}"
 fi
